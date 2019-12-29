fdfe43c642c9ecb5abf45d5eb9a77df99c5fbf41
claritymatrix <18380152997@163.com>
2019-12-29 21:28:39 +0800

    方便日后学习修改

--- daxue.py
+++ daxue.py
@@ -4,6 +4,21 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 
+# what:获取指定url网页
+
+# why:用于筛选出指定数据
+
+# who: url
+
+# when:当调用这个函数的时候
+
+# where:适用于数据都在一页上
+
+
+# how: 使用requests 库 获取response类数据获取整个网页html代码
+
+
+# how much: 可以使用r.text[0:500] 之类的限定看看取的数据怎么样。
 
 def getHtmlText(url):
 
@@ -15,7 +30,19 @@ def getHtmlText(url):
     except:
         return ""
 
+#what: 解析html 网页
 
+# why: 选出需要的数据
+
+# who: beautifulSoup 类
+
+# when:当调用这个函数的时候
+
+# how: 使用 html.parser
+
+# where: 所有数据在一个页面中
+
+# how much: 目标数量可以控制，复杂程度有限
 def fillUnivList(ulist,html):
 
     soup = BeautifulSoup(html,"html.parser")
@@ -25,7 +52,7 @@ def fillUnivList(ulist,html):
             tds = tr('td')
             ulist.append([tds[0].string, tds[1].string, tds[2].string])
 
-
+# 格式化输出数据
 
 
 def printUnivlist(ulist,num):
